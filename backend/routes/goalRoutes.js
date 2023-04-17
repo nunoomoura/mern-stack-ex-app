2d2642ab553057d46f7f98725e5c31f22efbdeaa
nunoomoura <nmoura49@gmail.com>
2023-04-17 23:56:11 +0100

    User API auth

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { protect } = require('../middleware/authMiddleware')
+
 const { 
     getGoals,
     setGoal,
@@ -7,7 +9,7 @@ const {
     deleteGoal
 } = require('../controllers/goalController')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router
